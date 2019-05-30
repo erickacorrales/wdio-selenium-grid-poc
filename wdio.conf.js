62b870384c57c14737a69cec1e60a0f98ff6053e
Ericka Corrales Anticona <erickacorrales@gmail.com>
2019-05-30 16:41:39 +0200

    add configuration circle-ci

--- wdio.conf.js
+++ wdio.conf.js
@@ -84,6 +84,11 @@ exports.config = {
         maxInstances: 1,
         //
         browserName: 'chrome',
+        'goog:chromeOptions': {
+            // to run chrome headless the following flags are required
+            // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
+            args: ['--headless', '--disable-gpu'],
+        }
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
